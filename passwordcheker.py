8e249e2017e9896fa4b302b8133bb63bbb6f1b8a
Ammar Khan <164665896+code-with-ammarkhan@users.noreply.github.com>
2025-03-05 13:46:24 +0500

    passwordcheker.py

--- passwordcheker.py
+++ passwordcheker.py
@@ -20,13 +20,20 @@ st.markdown(
 
     .animated-heading {
         text-align: center;
-        font-size: 32px;
+        font-size: 28px;
         font-weight: bold;
         animation: moveText 2s infinite alternate ease-in-out;
+        color: #333;
     }
 
     .stTextInput {width: 60% !important; margin: auto; }
-    .stButton button {width: 50%; background-color: #4CAF50; color: white; font-size: 18px; }
+    .stButton button {
+        width: 50%;
+        background-color: #4CAF50;
+        color: white;
+        font-size: 16px;
+        border-radius: 8px;
+    }
     .stButton button:hover { background-color: #45a049; }
     </style>
     """,
@@ -38,7 +45,7 @@ st.markdown("<h1 class='animated-heading'>🔐 Password Strength Checker</h1>", 
 
 # Centered Subtitle
 st.markdown(
-    "<h4 style='text-align: center; font-size: 18px; font-weight: bold;'>"
+    "<h4 style='text-align: center; font-size: 16px; font-weight: bold;'>"
     "Enter your password below to check its security level. 🔎"
     "</h4>", 
     unsafe_allow_html=True
@@ -92,4 +99,30 @@ if st.button("Check Password Strength"):
         st.error("🚫 Please enter a password first! ❗")  
 
 # Footer
-st.mark
+st.markdown(
+    """
+    <style>
+    @keyframes gradientText {
+        0% { color: rgb(148, 11, 98); }
+        100% { color: #00c9ff; }
+    }
+    
+    .footer {
+        text-align: center;
+        margin-top: 50px;
+        padding: 20px;
+        font-size: 16px;
+        font-weight: 900;
+    }
+    
+    .footer b {
+        display: inline-block;
+        font-size: 18px;
+        font-weight: 900;
+        animation: gradientText 2s infinite alternate;
+    }
+    </style>
+    <div class='footer'><b>Developed by ©️CodeWithAmmar</b></div>
+    """,
+    unsafe_allow_html=True
+)
